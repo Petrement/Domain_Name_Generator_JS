d6b0cf92dcf6e29a33a9fff7416039c4be629c97
Petrement <lspetrement@gmail.com>
2022-11-12 11:53:44 +0000

    Finished Excercise

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//window.onload = function() {
+//write your code here
+//console.log(Hello World!);
+//};
+
+let pronoun = ["the", "our", "we", "they", "a"];
+let adj = ["great", "awsome", "big", "small", "red"];
+let noun = ["baby", "dog", "woman", "man", "lady"];
+let dom = [".com", ".es", ".io", ".net"];
+
+let domains = document.querySelector("#webs");
+let content = "";
+
+for (let a = 0; a < pronoun.length; a++) {
+  for (let b = 0; b < adj.length; b++) {
+    for (let c = 0; c < noun.length; c++) {
+      for (let d = 0; d < dom.length; d++) {
+        //console.log(`${pronoun[a]}${adj[b]}${noun[c]}${dom[d]}`);
+        content += `${pronoun[a]}${adj[b]}${noun[c]}${dom[d]}<br>`;
+      }
+    }
+  }
+}
+domains.innerHTML = content;
